e92f3ede23d6f04b38863ea2387bf59e90f87b05
Oguzhan Ulaser <oguzhan.ulsr@gmail.com>
2026-02-24 18:32:41 +0300

    chore: initial project setup with Vite + React

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+return (
+<div>
+<h1>Web Tasarimi ve Programlama</h1>
+<h2>LAB-1</h2>
+<p>Ad Soyad: [Oğuzhan Ulaşer]</p>
+<p>Ogrenci No: [230542014]</p>
+</div>
+)
+}
+ export default App
